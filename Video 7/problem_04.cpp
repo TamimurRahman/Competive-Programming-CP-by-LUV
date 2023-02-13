467b623711ce22ecb144c54a31af2fe74ff5759d
TamimurRahman <tamimr008@gmail.com>
2023-02-14 02:02:44 +0600

    getline function use

---
+++ Video 7/problem_04.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string str,str2,str3;
+getline(cin,str);
+getline(cin,str2);
+cin>>str3;
+cout<<str<<endl<<str2<<"\n"<<str3<<"\n";
+return 0;
+}
