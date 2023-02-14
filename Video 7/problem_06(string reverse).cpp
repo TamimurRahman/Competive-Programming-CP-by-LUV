59deb59aa5e75774c9befb1817dbb734301db5e6
TamimurRahman <tamimr008@gmail.com>
2023-02-14 10:38:31 +0600

    change reverse system using string concatination

--- Video 7/problem_06(string reverse).cpp
+++ Video 7/problem_06(string reverse).cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string str;
+    string str,str_rev;
     getline(cin,str);
     int str_len = str.length();
     for(int i=str_len-1;i>=0;i--){
-        cout<<str[i];
-    }cout<<"\n";
+       str_rev = str_rev+str[i];
+    }cout<<str_rev<<endl;
 return 0;
 }
